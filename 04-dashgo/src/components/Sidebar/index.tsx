89ca8091baee4450e85518fce3678eb0aece3997
Ericlys Moreira <ericlysm@gmail.com>
2022-04-20 12:35:38 -0300

    separando componentes

---
+++ 04-dashgo/src/components/Sidebar/index.tsx
@@ -0,0 +1,21 @@
+import { Box, Icon, Link, Stack, Text } from "@chakra-ui/react";
+import { RiContactsBookLine, RiDashboardFill, RiGitMergeLine, RiInputMethodLine } from "react-icons/ri";
+import { NavLink } from "./NavLink";
+import { NavSection } from "./NavSection";
+
+export function Sidebar() {
+  return(
+    <Box as="aside" w="64" mr="8">
+      <Stack spacing="12" align="flex-start">
+        <NavSection title="GERAL">
+           <NavLink icon={RiDashboardFill}>Dashboard</NavLink>
+           <NavLink icon={RiContactsBookLine}>Usuários</NavLink>
+        </NavSection>
+        <NavSection title="AUTOMAÇÃO">
+          <NavLink icon={RiInputMethodLine}>Formulários</NavLink>
+          <NavLink icon={RiGitMergeLine}>Automação</NavLink>
+        </NavSection>
+      </Stack>
+    </Box>
+  )
+}
