b8cb031a8c776fb15b15c0c90d537570c7a0d68f
Ericlys Moreira <ericlysm@gmail.com>
2022-05-08 16:11:35 -0300

    feat: criando hook de permissões

--- 05-nextauth/src/pages/dashboard.tsx
+++ 05-nextauth/src/pages/dashboard.tsx
@@ -1,6 +1,6 @@
-import { destroyCookie } from "nookies";
 import { useContext, useEffect } from "react"
 import { AuthContext } from "../contexts/AuthContext"
+import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
 import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
@@ -8,12 +8,20 @@ import { withSSRAuth } from "../utils/withSSRAuth";
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
 
+  const userCanSeeMetrics = useCan({
+    roles: ['administrator', 'editor']
+  })
+
   useEffect(() => {
     api.get('/me').then(response => console.log(response))
   }, [])
 
   return(
-    <h1>Dashboard: {user?.email}</h1>
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+
+      { userCanSeeMetrics && <div>Métricas</div>}
+    </>
   )
 }
 
