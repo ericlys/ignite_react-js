eb18325b26b87bbc7519f073ddd1e9b6d3b94394
Ericlys Moreira <ericlysm@gmail.com>
2022-05-09 23:48:26 -0300

    feat: Broadcast de logout

--- 05-nextauth/src/pages/dashboard.tsx
+++ 05-nextauth/src/pages/dashboard.tsx
@@ -6,7 +6,7 @@ import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext);
+  const { user, signOut } = useContext(AuthContext);
 
   useEffect(() => {
     api.get('/me').then(response => console.log(response))
@@ -15,6 +15,8 @@ export default function Dashboard() {
   return(
     <>
       <h1>Dashboard: {user?.email}</h1>
+
+      <button onClick={signOut}>Sign out</button>
 
       <Can permissions={['metrics.list']}>
         <div>Métricas</div>
@@ -27,7 +29,7 @@ export const getServerSideProps = withSSRAuth(async (ctx) => {
   const apiClient = setupAPIClient(ctx);
 
   const response = await apiClient.get('/me');
-  console.log( response.data );
+  // console.log( response.data );
   return {
     props: {}
   }
