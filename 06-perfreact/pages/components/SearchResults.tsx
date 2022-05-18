12425f4dbcc15cc3f336a7f08a58faa9416fbe94
Ericlys Moreira <ericlysm@gmail.com>
2022-05-17 22:10:14 -0300

    feat: useMemo

--- 06-perfreact/pages/components/SearchResults.tsx
+++ 06-perfreact/pages/components/SearchResults.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
@@ -9,11 +10,20 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({ results }: SearchResultsProps) {
+
+  const totalPrice = useMemo( () => {
+    return results.reduce(( total, product) => {
+      return total + product.price;
+    }, 0)
+  }, [results])
+
   return(
     <div>
+      <h2>{totalPrice}</h2>
+
       {results.map(product => {
         return(
-          <ProductItem product={product}/>
+          <ProductItem key={product.id} product={product}/>
         )
       })}
     </div>
